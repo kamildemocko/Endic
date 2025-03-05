691edb0c4c06e10b3b39aa326a7aef9c18a709fa
Kamil Democko <kamil.democko@gmail.com>
2025-03-05 21:32:41 +0100

    args + version

--- src/args.rs
+++ src/args.rs
@@ -1,20 +1,28 @@
 use std::env;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub fn get_args() -> (bool, String) {
     let args = env::args().skip(1);
     let mut flag_match_word = false;
     let mut words: Vec<String> = Vec::new();
 
     if args.len() == 0 {
-        eprintln!("usage: script [-m | --match] QUERY");
-        eprintln!("\tQUERY = Word to search for");
-        eprintln!("\t-m | --match = Whole word search");
+        println!("Usage: ");
+        println!("endic [--match | --version] QUERY");
+        println!(" >> QUERY = Word to search for");
+        println!(" >> --match   | -m = Whole word search");
+        println!(" >> --version | -v = Shows app version");
 
-        std::process::exit(1);
+        std::process::exit(0);
     }
 
     for word in args {
-        if word == ("-m") || word == "--match" {
+        if word == "-v" || word == "--version" {
+            println!("endic v{VERSION}");
+
+            std::process::exit(0);
+        } else if word == "-m" || word == "--match" {
             flag_match_word = true;
             continue
         }
